1a5f27ad6b03f5c5a550787078c103534954e9c3
Suhrob Muborakshoev <suhrob.m89@gmail.com>
2023-02-27 23:17:58 -0800

    Added removeItemFromCart and add action for Component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,30 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const PRODUCTS = [
+  { id: "p1", price: 6, title: "My First book", description: "The first book" },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second book",
+    description: "The second book",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
